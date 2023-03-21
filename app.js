e99b23c6222fc99375c0067f10c420041dad8e25
kirill <kirill.salikhov@gmail.com>
2023-03-21 13:19:38 +0400

    touched app.js to trigger github ci

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Koa = require('koa');
 
 const port = 8080;
 const greetingName = process.env.GREETING_NAME ?? '__not_set__';
+
 new Koa()
     .use(async ctx => {
         ctx.body = `Hello, ${greetingName}`;
