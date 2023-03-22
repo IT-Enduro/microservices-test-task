a102a6b82da3444848270e95d3d78e36f4d408ac
anuritto <anur.usmanov@gmail.com>
2023-03-22 21:23:37 +0400

    fix: fastify host

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,10 @@ server.get("/", async (request, reply) => {
   return `Hello, ${process.env.GREETING_NAME}`;
 });
 
-server.listen(8080, (err, address) => {
+server.listen(8080, "0.0.0.0", (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
   console.log(`Started server at ${address}`);
-})
+});
