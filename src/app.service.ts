2efe97a3507a874ccc12811558b3d401e3c58c95
Victoria Akhmatova <toriatovawebdev@gmail.com>
2023-03-24 12:30:32 +0400

    Fix typo

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(name: string): string {
-    return `Hello, ${name}!`;
+    return `Hello, ${name}`;
   }
 }
