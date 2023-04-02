67c8975d8d74b7b52833c6272bc047a346103835
bendrikovski <bendrikovski@gmail.com>
2023-04-02 23:29:35 +0400

    Creation of project and adjusting of Docker

---
+++ src/main/java/com/edu/entrancetask/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.edu.entrancetask.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+    @Value("${greeting.user}")
+    private String user;
+
+    @GetMapping
+    String hello() {
+        return String.format("Hello, %s", user);
+    }
+}
